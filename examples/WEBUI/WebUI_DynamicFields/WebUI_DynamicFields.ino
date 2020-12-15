9170824a3da5af0d31456ac3acf5a3d80dd6362c
philbowles <philbowles2012@gmail.com>
2020-12-15 19:46:31 +0100

    dynamic fields example

--- examples/WEBUI/WebUI_DynamicFields/WebUI_DynamicFields.ino
+++ examples/WEBUI/WebUI_DynamicFields/WebUI_DynamicFields.ino
@@ -1,38 +1,50 @@
-
 #include<H4Plugins.h>
 H4_USE_PLUGINS(115200,20,false) // Serial baud rate, Q size, SerialCmd autostop
 
+boolean condition=true;
+
 boolean boolData(){ return random(0,50) > 25; }
 
-void userBoolAction(const string& name,const string& b){ 
-  h4asws.uiMessage("%s was clicked! %s\n",CSTR(name),CSTR(b));
-}
+void userBoolAction(const string& name,const string& b){ h4asws.uiMessage("%s was clicked! %s\n",CSTR(name),CSTR(b)); }
 
-H4_TIMER  millisTimer;
+H4_TIMER ub,msg,txt,cond;
 
-// whenever a user browses the UI
-void onViewers(){
-    // create your own UI fields and "setter" functions
-    h4asws.uiAddLabelText("User Text",[]{ return "some value"; });  
-    h4asws.uiAddBoolean("User Bool",boolData); // no action function = not clickable  
-    h4asws.uiAddBoolean("User Bool 1",boolData,userBoolAction);  
-    h4asws.uiAddBoolean("User Bool 2",boolData,userBoolAction);  
-    h4asws.uiAddLabelNumeric("Millis",millis);
-    // setup conditions for updating fields (if required)
-    h4.everyRandom(15000,20000,[]{ h4asws.uiSetBoolean("User Bool",boolData); });
-    h4.everyRandom(15000,20000,[]{ h4asws.uiSetBoolean("User Bool 1",boolData); });
-    h4.everyRandom(15000,20000,[]{ h4asws.uiSetBoolean("User Bool 2",boolData); });
-    h4.everyRandom(25000,30000,[]{ h4asws.uiMessage("IMPORTANT ANNOUNCEMENT"); });
-    millisTimer=h4.every(1000,[]{ h4asws.uiSetLabelNumeric("Millis",millis); }); 
-}
+void onViewers(); // necessary fwd reference
+// Release global resources when ui is no longer required as all browsers closed
+void onNoViewers(){ h4.cancel({ub,msg,txt,cond}); }
 
-// when ui is no longer required as all browsers closed
-void onNoViewers(){  h4.cancel(millisTimer); }
+void onMQTTError(uint8_t e,int info){ Serial.printf("USER MQTT ERROR %d INFO %d\n",e,info); }
 
 H4P_GPIOManager h4gm;
 H4P_WiFi h4wifi("XXXXXXXX","XXXXXXXX","webui");
-H4P_AsyncMQTT h4mqtt("192.168.1.4",1883);
+H4P_AsyncMQTT h4mqtt("192.168.1.21",1883,"example","pangolin");
 H4P_AsyncWebServer h4asws(onViewers,onNoViewers);
+H4P_Heartbeat h4hb([]{ h4asws.uiSync("Uptime"); });
+H4P_ConditionalSwitch h4onof(LED_BUILTIN,ACTIVE_LOW,OFF,[](bool b){ return condition; });
+H4P_UPNPServer h4upnp("V0.5.9 Active UI Tester");
 
-H4P_ConditionalSwitch h4onof(RELAY_BUILTIN,ACTIVE_HIGH,OFF,[](bool b){ return true; });
-H4P_UPNPServer h4upnp("V0.5.6 Active UI Tester");
+void onViewers(){
+    // setup conditions for updating fields (if required)
+    ub=h4.everyRandom(15000,20000,[]{ // Normal method is to sync: calls the "setter" function provided to uiAddxxx
+      h4asws.uiSync("User Bool");
+      h4asws.uiSync("User Number");
+    });
+    txt=h4.onceRandom(17000,25000,[]{ h4asws.uiSetLabelText("User Text",H4P_VERSION); }); // don't call uiAddxxx "setter", use arbitrary value
+    msg=h4.everyRandom(25000,30000,[]{ h4asws.uiMessage("IMPORTANT ANNOUNCEMENT"); });
+    cond=h4.everyRandom(15000,30000,[]{
+      condition=!condition; 
+      h4onof.syncCondition();
+      h4asws.uiMessage("CONDITION NOW %sARMED",condition ? "":"DIS");
+    });
+}
+
+void h4setup(){
+//  create your own UI fields and "setter" functions
+    h4asws.uiAddLabelNumeric("User Number",[]{ return random(42,666); });  
+    h4asws.uiAddLabelText("User Text",[]{ return "INITIAL"; }); 
+    h4asws.uiAddBoolean("User Bool",boolData); // no action: function = not clickable  
+    h4asws.uiAddBoolean("User Bool 1",boolData,userBoolAction);  
+    h4asws.uiAddBoolean("User Bool 2",boolData,userBoolAction);  
+    h4asws.uiAddLabelText("Uptime",H4P_Heartbeat::upTime);
+    h4asws.uiAddGPIO(LED_BUILTIN);  
+}
